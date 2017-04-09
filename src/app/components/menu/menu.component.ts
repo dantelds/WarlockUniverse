0b3b50fbf9445937e8d09644f5d065dab5dc9272
David Alejandro Idarraga Martinez <David Alejandro Idarraga Martinez>
2017-04-09 18:01:47 +0200

    add spanish language

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,5 +1,7 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import { ILink } from "../../interfaces/link";
+import { TranslateService } from '../../services/language.service';
+
 
 @Component({
   selector: 'menu-app',
@@ -8,30 +10,32 @@ import { ILink } from "../../interfaces/link";
 })
 export class MenuComponent {
   @Output() onModuleClicked: EventEmitter<ILink>=new EventEmitter<ILink>();
+
+
   items: ILink[]=[
     {
       href:'warlock',
-      text:'Personaje'
+      text:this._translate.instant('menu-warlock')
     },
     {
       href:'coming-soon',
-      text:'Escuela'
+      text:this._translate.instant('menu-school')
     },
     {
       href:'coming-soon',
-      text:'Mazmorras'
+      text:this._translate.instant('menu-dungeons')
     },
     {
       href:'coming-soon',
-      text:'Duelos'
+      text:this._translate.instant('menu-arena')
     },
     {
       href:'coming-soon',
-      text:'Tienda'
+      text:this._translate.instant('menu-store')
     },
   ];
   currentItem:ILink;
-  constructor(){
+  constructor(private _translate: TranslateService){
     this.currentItem = this.items[0];
   }
   onSelectItem(selectedItem:ILink):void{
