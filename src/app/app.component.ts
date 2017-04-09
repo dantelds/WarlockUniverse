9827f4f1f89d6e57c0a838ea909bac2b4f2c09a2
David Alejandro Idarraga Martinez <David Alejandro Idarraga Martinez>
2017-04-09 18:34:16 +0200

    change language service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,26 +8,14 @@ import { Router } from '@angular/router';
     templateUrl:'app/app.component.html'
 })
 export class AppComponent {
-  public supportedLanguages: any[];
   logged:boolean = false;
+
   constructor(private _translate: TranslateService, private LoginService: LoginService, private router: Router) {
     this.router.navigate(['/login']);
     LoginService.loginManagerEmmiter$.subscribe(User => {
       this.logged = true;
       this.router.navigate(['/warlock']);
     });
-  }
-
-  ngOnInit() {
-    var language:string=navigator.language;
-    this.supportedLanguages = [
-      { display: 'English', value: 'en' },
-      { display: 'Español', value: 'es'}
-    ];
-    if(language !== 'es' && language !== 'en'){
-      language = 'en';
-    }
-    this.selectLang(language);
   }
 
   isCurrentLang(lang: string) {
@@ -37,8 +25,6 @@ export class AppComponent {
   selectLang(lang: string) {
     this._translate.use(lang);
   }
-
-
 
   onModuleClicked(module:ILink){
     console.log('soy el modulo pulsado',module);
--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,6 +1,6 @@
-import { Component, Output, EventEmitter } from '@angular/core';
-import { ILink } from "../../interfaces/link";
-import { TranslateService } from '../../services/language.service';
+import {Component, Output, EventEmitter} from '@angular/core';
+import {ILink} from "../../interfaces/link";
+import {TranslateService} from '../../services/language.service';
 
 
 @Component({
@@ -9,37 +9,43 @@ import { TranslateService } from '../../services/language.service';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
-  @Output() onModuleClicked: EventEmitter<ILink>=new EventEmitter<ILink>();
+  @Output() onModuleClicked: EventEmitter<ILink> = new EventEmitter<ILink>();
 
 
-  items: ILink[]=[
-    {
-      href:'warlock',
-      text:this._translate.instant('menu-warlock')
-    },
-    {
-      href:'school',
-      text:this._translate.instant('menu-school')
-    },
-    {
-      href:'coming-soon',
-      text:this._translate.instant('menu-dungeons')
-    },
-    {
-      href:'coming-soon',
-      text:this._translate.instant('menu-arena')
-    },
-    {
-      href:'coming-soon',
-      text:this._translate.instant('menu-store')
-    },
-  ];
-  currentItem:ILink;
-  constructor(private _translate: TranslateService){
+  items: ILink[] = null;
+  currentItem: ILink;
+
+  constructor(private _translate: TranslateService) {
+    this.items = [
+      {
+        href: 'warlock',
+        text: this._translate.instant('menu-warlock')
+      },
+      {
+        href: 'school',
+        text: this._translate.instant('menu-school')
+      },
+      {
+        href: 'coming-soon',
+        text: this._translate.instant('menu-dungeons')
+      },
+      {
+        href: 'coming-soon',
+        text: this._translate.instant('menu-arena')
+      },
+      {
+        href: 'coming-soon',
+        text: this._translate.instant('menu-store')
+      },
+    ];
     this.currentItem = this.items[0];
+    console.log('items-------->', this.items);
+    console.log("this._translate.instant('menu-warlock')-------->", this._translate.instant('menu-warlock'));
+    console.log("this._translate.instant('user-login')-------->", this._translate.instant('user-login'));
   }
-  onSelectItem(selectedItem:ILink):void{
-    this.currentItem=selectedItem;
+
+  onSelectItem(selectedItem: ILink): void {
+    this.currentItem = selectedItem;
     this.onModuleClicked.emit(selectedItem);
   }
 }
