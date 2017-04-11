5b5ca0ce81b382f0c3b10ab34215cab1135347f2
David Alejandro Idarraga Martinez <David Alejandro Idarraga Martinez>
2017-04-11 21:39:49 +0200

    create input and select component

--- src/app/modules/sign-up/sign-up.component.ts
+++ src/app/modules/sign-up/sign-up.component.ts
@@ -4,19 +4,129 @@ import {TranslateService} from '../../services/language.service';
 import {Md5} from 'ts-md5/dist/md5';
 
 import {IUser} from "../../interfaces/user";
+import {IInput} from "../../interfaces/input";
+import {ISelect} from "../../interfaces/select";
 @Component({
   selector: 'login',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent {
-
-  user: IUser = null;
+  user: IUser = this.GeneralService.returnEmptyUser();
   error: string = null;
   schoolText: string = null;
 
+  userInput: IInput = {
+    id: 'su_username',
+    extraClass: null,
+    error: this._translate.instant('user-username-required'),
+    label: this._translate.instant('user-username'),
+    model: '',
+    name: 'su_username',
+    placeholder: this._translate.instant('user-username'),
+    required: true,
+    type: 'text'
+  };
+
+  passwordInput: IInput = {
+    id: 'su_password',
+    extraClass: null,
+    error: this._translate.instant('user-password-required'),
+    label: this._translate.instant('user-password'),
+    model: '',
+    name: 'su_password',
+    placeholder: this._translate.instant('user-password'),
+    required: true,
+    type: 'password'
+  };
+
+  nameInput: IInput = {
+    id: 'su_name',
+    extraClass: null,
+    error: this._translate.instant('user-name-required'),
+    label: this._translate.instant('user-name'),
+    model: '',
+    name: 'su_name',
+    placeholder: this._translate.instant('user-name'),
+    required: true,
+    type: 'text'
+  };
+
+  lastNameInput: IInput = {
+    id: 'su_lastName',
+    extraClass: null,
+    error: this._translate.instant('user-lastName-required'),
+    label: this._translate.instant('user-lastName'),
+    model: '',
+    name: 'su_lastName',
+    placeholder: this._translate.instant('user-lastName'),
+    required: true,
+    type: 'text'
+  };
+
+  genderSelect: ISelect = {
+    id:'su_gender',
+    extraClass: '',
+    error: this._translate.instant('user-gender-required'),
+    label: this._translate.instant('user-gender'),
+    model: '',
+    name: 'su_gender',
+    options:[
+      {
+        text:this._translate.instant('user-select-gender'),
+        value:''
+      },{
+        text:this._translate.instant('Male'),
+        value:'Male'
+      },{
+        text:this._translate.instant('Female'),
+        value:'Female'
+      }],
+    required: true
+  };
+
+  birthDateInput: IInput = {
+    id: 'su_birthDate',
+    extraClass: null,
+    error: this._translate.instant('user-birthDate-required'),
+    label: this._translate.instant('user-birthDate'),
+    model: '',
+    name: 'su_birthDate',
+    placeholder: this._translate.instant('user-birthDate'),
+    required: true,
+    type: 'date'
+  };
+
+  schoolSelect: ISelect = {
+    id:'su_school',
+    extraClass: '',
+    error: this._translate.instant('user-school-required'),
+    label: this._translate.instant('user-school'),
+    model: '',
+    name: 'su_school',
+    options:[
+      {
+        text:this._translate.instant('user-select-school'),
+        value:''
+      },{
+        text:this._translate.instant('earth'),
+        value:'earth'
+      },{
+        text:this._translate.instant('fire'),
+        value:'fire'
+      },{
+        text:this._translate.instant('water'),
+        value:'water'
+      },{
+        text:this._translate.instant('wind'),
+        value:'wind'
+      }],
+    required: true
+  };
+
+
+
   constructor(private GeneralService: GeneralService, private _translate: TranslateService) {
-    this.user = this.GeneralService.returnEmptyUser();
   }
 
   setInitialAttributes(strength: number, magic: number, health: number) {
@@ -25,7 +135,8 @@ export class SignUpComponent {
     this.user.health = health;
   }
 
-  onChangeSchool() {
+  onSelectSchool(selectedOption:string){
+    this.user.school = selectedOption;
     switch (this.user.school) {
       case 'earth':
         this.schoolText = this._translate.instant('school-earth-advantages');
@@ -49,6 +160,7 @@ export class SignUpComponent {
     }
   }
 
+
   onSignUp(user: IUser) {
     this.error = null;
     this.GeneralService.user = user;
@@ -60,8 +172,18 @@ export class SignUpComponent {
   }
 
   signUp() {
-    var body: IUser = Object.assign({}, this.user);
-    body.password = Md5.hashStr(this.user.password).toString();
+    var body: IUser = {
+      username: this.userInput.model,
+      password: Md5.hashStr(this.passwordInput.model).toString(),
+      name: this.nameInput.model,
+      lastName: this.lastNameInput.model,
+      birthDate: this.birthDateInput.model,
+      school: this.schoolSelect.model,
+      gender: this.genderSelect.model,
+      health: this.user.health,
+      magic: this.user.magic,
+      strength: this.user.strength
+    };
     this.GeneralService.apiCall('post', 'sign-up', body)
       .subscribe((Response:any) => this.onSignUp(Response), (Error:any) => this.onError(Error));
   }
